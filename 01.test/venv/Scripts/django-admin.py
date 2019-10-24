5ad315dedc7cff43c8da74b1bfba1b274481e86b
Bwori <obriann21@gmail.com>
2019-10-24 15:48:26 +0300

    Initial commit

---
+++ 01.test/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\brianobuna\Pycham_Python_projects\Django-projects\01.test\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
